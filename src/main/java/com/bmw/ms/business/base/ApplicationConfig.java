8290fff22c3cb6d58846e4fc37161216b6ed7ebe
UNGERW <wolfgang.unger@nttdata.com>
2017-06-21 10:19:10 +0200

    changes on pom

--- src/main/java/com/bmw/ms/business/base/ApplicationConfig.java
+++ src/main/java/com/bmw/ms/business/base/ApplicationConfig.java
@@ -4,7 +4,6 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 //set the path to REST web services
-// one more comment
 @ApplicationPath("/api/v1")
 public class ApplicationConfig extends Application {
 
