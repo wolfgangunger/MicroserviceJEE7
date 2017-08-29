9a5672f580926c6ebcc9dc0d457ccd7524c1cf7e
Unger Wolfgang <XXX@nttdata.com>
2017-08-29 12:22:08 +0200

    changes

--- src/main/resources/insertData.sql
+++ src/main/resources/insertData.sql
@@ -7,13 +7,13 @@
  * Author:  UNGERW
  * Created: 02.06.2017
  */
-Insert into T_SalesOrder (id, description, order_number) values (1, 'test order', '001');
-Insert into T_SalesOrder (id, description, order_number) values (2, 'test order no 2', '002');
-Insert into T_SalesOrder (id, description, order_number) values (3, 'test order no 3', '003');
+Insert into T_SalesOrder (id, description, order_number, version) values (1, 'test order', '001',1);
+Insert into T_SalesOrder (id, description, order_number,version) values (2, 'test order no 2', '002',1);
+Insert into T_SalesOrder (id, description, order_number,version) values (3, 'test order no 3', '003',1);
 
-Insert into T_SalesOrderDetail (id, description, detail_number, item, order_id) values (4, 'order detal', 1, 'Laptop', 1);
-Insert into T_SalesOrderDetail (id, description, detail_number, item, order_id) values (5, 'order detal', 2, 'Iphone', 1);
-Insert into T_SalesOrderDetail (id, description, detail_number, item, order_id) values (6, 'order detal', 3, 'USB Stick', 1);
-Insert into T_SalesOrderDetail (id, description, detail_number, item, order_id) values (7, 'order detal', 1, 'Laptop', 2);
-Insert into T_SalesOrderDetail (id, description, detail_number, item, order_id) values (8, 'order detal', 2, 'Monitor', 2);
+Insert into T_SalesOrderDetail (id, description, detail_number, item, order_id,version) values (4, 'order detal', 1, 'Laptop', 1, 1);
+Insert into T_SalesOrderDetail (id, description, detail_number, item, order_id,version) values (5, 'order detal', 2, 'Iphone', 1,1);
+Insert into T_SalesOrderDetail (id, description, detail_number, item, order_id,version) values (6, 'order detal', 3, 'USB Stick', 1,1);
+Insert into T_SalesOrderDetail (id, description, detail_number, item, order_id,version) values (7, 'order detal', 1, 'Laptop', 2,1);
+Insert into T_SalesOrderDetail (id, description, detail_number, item, order_id,version) values (8, 'order detal', 2, 'Monitor', 2,1);
 
